86cb50a61a3e1b901de46ec8b47a27c67e721d95
Faiyaz Sarkar <faiyazvai999@gmail.com>
2026-06-05 14:17:19 +0600

    refactor(index): remove user management logic from server setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,7 @@
 import { createServer } from "http";
 import app from "./app.js";
 import { Server } from "socket.io";
-import {
-  createUser,
-  getUsersByRoom,
-  removeUserBySocketId,
-} from "./models/userModel.js";
+
 import { env } from "./config/env.js";
 
 const PORT = process.env.PORT || 3000;
@@ -15,58 +11,6 @@ const io = new Server(server, {
   cors: {
     origin: env.NODE_ENV === "development" ? "http://localhost:5173" : "/",
   },
-});
-
-io.on("connection", (socket) => {
-  console.log("A user connected");
-
-  socket.on("join_room", ({ username, roomId }) => {
-    socket.join(roomId);
-
-    createUser(username, roomId, socket.id);
-
-    const roomUsers = getUsersByRoom(roomId);
-    console.log(`Users in room ${roomId}:`, roomUsers);
-
-    io.to(roomId).emit("room_users", roomUsers);
-
-    socket.broadcast.to(roomId).emit("receive_message", {
-      sender: "System",
-      text: `${username} joined the room`,
-      timestamp: new Date().toLocaleTimeString(),
-    });
-  });
-
-  // Listen for messages
-  socket.on("chatMessage", (data) => {
-    const { sender, text, roomId } = data;
-
-    io.to(roomId).emit("receive_message", {
-      sender,
-      text,
-      roomId,
-      timestamp: new Date().toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-      }),
-    });
-  });
-
-  socket.on("disconnect", () => {
-    const user = removeUserBySocketId(socket.id);
-
-    if (user) {
-      const roomUsers = getUsersByRoom(user.roomId);
-
-      io.to(user.roomId).emit("room_users", roomUsers);
-
-      io.to(user.roomId).emit("receive_message", {
-        sender: "System",
-        text: `${user.username} left the room`,
-        timestamp: new Date().toLocaleTimeString(),
-      });
-    }
-  });
 });
 
 // Start server
