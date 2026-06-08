4b2fb15fd92941241f5a7afa63a00c82552f6c61
Faiyaz Sarkar <faiyazvai999@gmail.com>
2026-06-08 20:11:03 +0600

    refactor(message): update user ID retrieval to use getUserIdByClerkId

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -9,7 +9,7 @@ export const handleGetMessagesBetweenTwoUsers = async (
   req: Request,
   res: Response,
 ) => {
-  const currentUserId = req.user?.id!;
+  const currentUserId = await getUserIdByClerkId(req.user?.clerkId!);
   const otherUserId = await getUserIdByClerkId(req.params.clerkUserId);
   const messages = await getMessagesBetweenTwoUsers(currentUserId, otherUserId);
 
@@ -22,7 +22,7 @@ export const handleGetMessagesBetweenTwoUsers = async (
 };
 
 export const handleCreateMessage = async (req: Request, res: Response) => {
-  const senderId = req.user?.id!;
+  const senderId = await getUserIdByClerkId(req.user?.clerkId!);
   const receiverId = await getUserIdByClerkId(req.params.clerkUserId);
   const { text, image } = req.body;
 
