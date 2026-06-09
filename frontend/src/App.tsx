a7af2555d81cfe3539836d7b24da147111889244
Faiyaz Sarkar <faiyazvai999@gmail.com>
2026-06-09 17:17:42 +0600

    feat(auth): call useAuthReq hook in app.tsx file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,13 @@ import Join from "./pages/Join";
 import Chat from "./pages/Chat";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { useAuthReq } from "./hooks/useAuthReq";
 
 function App() {
+  const { isClerkLoaded } = useAuthReq();
+  if (!isClerkLoaded) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
       <Navbar />
